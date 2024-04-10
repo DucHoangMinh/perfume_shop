35a93ba8edc0a29c3b0f9a828768d1e07e1972f1
HoangMinhDuc <duchm@otani-trading.com>
2024-04-10 16:02:11 +0700

    add slider banner images

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,9 @@
+const slider_banner_images = [
+  "https://firebasestorage.googleapis.com/v0/b/perfumeshop-44117.appspot.com/o/assets%2Fslider-banner-1.jpg?alt=media&token=5c27d11a-6da8-4a62-b441-9a3e8fd41753",
+  "https://firebasestorage.googleapis.com/v0/b/perfumeshop-44117.appspot.com/o/assets%2Fslider-banner-2.png?alt=media&token=11007c1f-8431-4a37-8d09-04467d02ea9c",
+  "https://firebasestorage.googleapis.com/v0/b/perfumeshop-44117.appspot.com/o/assets%2Fslider-banner-3.png?alt=media&token=fd71c2d7-4f71-4c4b-a46c-92948cd563f6",
+  "https://firebasestorage.googleapis.com/v0/b/perfumeshop-44117.appspot.com/o/assets%2Fslider-banner-4.png?alt=media&token=03d0bdcf-7aa5-4830-880e-c10a5f3e2285"
+]
+export {
+  slider_banner_images
+}
